3d93d7a13685ca95ef0208e8fdeddebfc5822145
NandaSalsabillah <nandasalsabillah89@gmail.com>
2025-04-20 21:03:53 +0700

    Update tugas-3.py

--- Pratikum/pre-1-perulangan/tugas-3.py
+++ Pratikum/pre-1-perulangan/tugas-3.py
@@ -2,5 +2,5 @@ jumlah = int(input("Masukkan jumlah deret Fibonacci: "))
 a, b = 0, 1 # a = nilai pertama, b = nilai kedua
 
 for i in range(jumlah):
-    print(f"i ke-{i+1} -> {a}") 
+    print(a, end= ", ")
     a, b = b, a + b # rumus fibonacci
