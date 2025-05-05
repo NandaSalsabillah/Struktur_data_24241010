1468265af039fce262ad6fe670d771e747482b0b
NandaSalsabillah <nandasalsabillah89@gmail.com>
2025-05-05 20:46:49 +0700

    tuples.py

--- MODUL-1/tuples.py
+++ MODUL-1/tuples.py
@@ -45,4 +45,49 @@ angka = (10, 20, 30, 40)
 print(angka[1:3]) 
 print(angka[:2])
 print(angka[2:])
+print(angka[2:])
+
+# Operasi pada Tuple
+# penggabungan tuple
+print((1, 2) + (3, 4))
+
+# pengulangan tuple
+print(('A',) * 3)
+
+# cek panjang tuple
+data = (1, 2, 4, 5)
+print(len(data))
+
+# cek keanggotaan tuple
+print(3 in data)
+
+# Tuple Bersarang (Nested Tuple)
+t = (1, 2, (3, 4))
+print(t[2][0])  # Output: 3
+
+tuple1 = "aku", "mahasiswa", "PTI UNDIKMA"
+tuple2 = "selama", 3, "tahun"
+tuple3 = (tuple1, tuple2) # <- nested tuple
+
+print(tuple3)
+
+# tuple berisi list
+t = ([1,2,3,4], True)
+
+print (t)
+
+# Sequence Unpacking
+# mula-mula kita buat tuple seperti ini
+web = 123, "PTI UNDIKMA", "https://fsttundikma.id"
+
+# lalu di-unpacking
+id_web, nama, url = web
+
+# maka sekarang tiga variabel tersebut akan bernilai
+# sesuai yang ada di dalam tuple
+#
+# mari kita cetak
+print(id_web)
+print(nama)
+print(url)
 
